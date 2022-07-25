3783230c940b4320f2b315160b54371c4e2b3e3e
estebanorlandi4 <estebanorlandi4@gmail.com>
2022-07-25 16:56:42 -0300

    feat: Landing with Footer and Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 const Home: NextPage = () => {
   return (
@@ -13,12 +15,15 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Navbar />
+
       <main style={{ display: "grid", placeItems: "center", height: "100vh" }}>
         <div>
           <h1>DL3arn</h1>
         </div>
       </main>
-      <footer></footer>
+
+      <Footer />
     </div>
   );
 };
